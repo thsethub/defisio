54d5236be65c567022d240ac4e3909fb09c2e15e
Thiago <thsethub@gmail.com>
2025-06-04 13:05:09 -0300

    Atualização na porta de comunicação com o banco de dados

--- src/main/java/com/ufpe/defisio/linfedemapp/dto/Paciente/HormonoterapyDTO.java
+++ src/main/java/com/ufpe/defisio/linfedemapp/dto/Paciente/HormonoterapyDTO.java
@@ -9,6 +9,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class HormonoterapyDTO {
+    //Tipo
     private String type;
+    //Duração
     private String duration;
 }
